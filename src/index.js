0fa79b65621b5677312c82182ab01c793689aae4
Daniyal Kargari <daniyal4v@gmail.com>
2021-12-21 15:26:10 +0330

    add global style

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import "normalize.css/normalize.css";
 import "bootstrap/dist/css/bootstrap.rtl.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
+import "assets/style.scss";
 ReactDOM.render(
   <React.StrictMode>
     <App />
